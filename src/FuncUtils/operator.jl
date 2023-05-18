86d2f2560e30b7d0213f78a501123ec0fe7f22fc
Koh Hashimoto <koh.hashimoto@gmail.com>
2023-05-18 17:05:35 +0900

    fix: |>

--- src/FuncUtils/operator.jl
+++ src/FuncUtils/operator.jl
@@ -2,12 +2,16 @@
 # functionize
 #################################
 
-Base.:|>(x, f) = functionize(f)(x)
+Base.:|>(x, f::Symbol) = x |> functionize(f)
+
+Base.:|>(x, f::Union{Tuple, AbstractArray, Base.Generator, Base.ValueIterator}) = x |> functionize(f)
 
 Base.:|(x, f) = functionize(f)(x)
 
 Base.:/(f, g) = functionize(g) ∘ functionize(f)
 
 Base.:~(f) = functionalize(f)
+
 Base.:-(f) = a -> functionalize(f)(a...)
+
 Base.:+(f) = a -> functionalize(f).(a)
