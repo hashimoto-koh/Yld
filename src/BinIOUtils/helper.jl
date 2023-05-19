02b7d1099017265eaef821ce74665555496573fa
Koh Hashimoto <koh.hashimoto@gmail.com>
2023-05-19 11:50:09 +0900

    tiny

--- src/BinIOUtils/helper.jl
+++ src/BinIOUtils/helper.jl
@@ -5,6 +5,6 @@
 _force_ext(filename::AbstractString, ext::AbstractString, forcename::Bool) =
         (forcename || "." * ext == splitext(filename)[2]
          ? filename : filename * "." * ext)
+
 _force_ext(filename::AbstractString, ext::AbstractString, forcename) =
     _force_ext(filename, ext, toBool(forcename))
-
